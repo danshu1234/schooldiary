13d9fd6f82891f9512f90d29d1f72815bf2c2f1a
Daniil Shundeev <danya.shundeev@internet.ru>
2024-11-27 21:04:49 +0500

    *_*

--- src/app/[subject]/page.tsx
+++ src/app/[subject]/page.tsx
@@ -1,17 +1,35 @@
-import { FC } from "react"; 
-import Homeworks from "./Homeworks"; 
-interface Params { 
-    subject: string; 
-} 
+'use client'
 
-interface Props { 
-    params: Params; 
-} 
+import { FC, useEffect, useState } from "react";
+import Homeworks from "./Homeworks";
 
-const DayHomework: FC<Props> = ({ params }) => { 
-    return ( 
-        <Homeworks params={params.subject} /> 
-    ); 
-}; 
+interface Params {
+    subject: string;
+}
+
+interface Props {
+    params: Promise<Params>;
+}
+
+const DayHomework: FC<Props> = ({ params }) => {
+    const [subject, setSubject] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchParams = async () => {
+            try {
+                const resolvedParams = await params; 
+                setSubject(resolvedParams.subject); 
+            } catch (error) {
+                console.error("Ошибка при получении параметров:", error);
+            }
+        };
+
+        fetchParams(); 
+    }, [params]);
+
+    return (
+        subject ? <Homeworks params={subject} /> : <div>Loading...</div>
+    );
+};
 
 export default DayHomework;
